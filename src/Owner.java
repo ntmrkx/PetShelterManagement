075d5dfc7f2b9256e49dd1dcb5caabc60dbe1a5e
dirolnahh <ntmrkx@gmail.com>
2026-04-27 13:23:12 +0200

    owner class created TODO

--- src/Owner.java
+++ src/Owner.java
@@ -1,6 +1,15 @@
 public class Owner {
     private String name;
     private String email;
-    private String adress;
+    private String address;
     private String phone;
+
+    public Owner(String name, String email, String address, String phone) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    // getters & setters
 }
